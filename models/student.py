f1a33446e2950b97c16b490661526df381bb71e8
collo <collinskipkorir430@gmail.com>
2026-02-20 12:48:34 +0300

    student details added

--- models/student.py
+++ models/student.py
@@ -17,3 +17,52 @@ class UniversityStudent(models.Model):
         ondelete='cascade',
         required=True
     )
+    fee_ids = fields.One2many('university.fee', 'student_id')
+
+    total_fee = fields.Float(compute='_compute_fee')
+    total_paid = fields.Float(compute='_compute_fee')
+    fee_balance = fields.Float(compute='_compute_fee')
+
+    @api.depends('fee_ids.amount', 'fee_ids.paid_amount')
+    def _compute_fee(self):
+        for student in self:
+            total = sum(f.amount for f in student.fee_ids)
+            paid = sum(f.paid_amount for f in student.fee_ids)
+
+            student.total_fee = total
+            student.total_paid = paid
+            student.fee_balance = total - paid
+
+    course_id = fields.Many2one('university.course')
+    result_ids = fields.One2many('student.result', 'student_id')
+
+    total_credits = fields.Float(compute='_compute_gpa')
+    total_points = fields.Float(compute='_compute_gpa')
+    gpa = fields.Float(compute='_compute_gpa', store=True)
+
+    @api.depends('result_ids.marks', 'result_ids.credit_units')
+    def _compute_gpa(self):
+        for student in self:
+            total_points = 0.0
+            total_credits = 0.0
+
+            for result in student.result_ids:
+                grade_point = 0
+
+                if result.marks >= 70:
+                    grade_point = 4
+                elif result.marks >= 60:
+                    grade_point = 3
+                elif result.marks >= 50:
+                    grade_point = 2
+                elif result.marks >= 40:
+                    grade_point = 1
+                else:
+                    grade_point = 0
+
+                total_points += grade_point * result.credit_units
+                total_credits += result.credit_units
+
+            student.total_points = total_points
+            student.total_credits = total_credits
+            student.gpa = total_points / total_credits if total_credits else 0
